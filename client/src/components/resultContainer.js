58dd7f85a1c043271f82807a5b510dc1183ff4fc
Ben Stephens <ben_j_stephens@hotmail.com>
2020-10-05 18:35:03 +1100

    got save to work, included () ran all the time

--- client/src/components/resultContainer.js
+++ client/src/components/resultContainer.js
@@ -4,42 +4,50 @@ import API from "../util/API"
 
 import { Row, Col } from "react-bootstrap";
 import Button from "react-bootstrap/Button"
-import Container from "react-bootstrap/Container"
 
 function ResultContainer(props) {
 
-    const handleClick = () => {
+    const handleSave = () => {
         API.saveBook({
             title: props.title,
             authors: props.authors,
             description: props.description,
             image: props.image,
             link: props.link
-        }).catch(
+        })
+        .then(console.log("successfully added a book"))
+        .catch(
             err => console.error(err)
         )
     } 
 
     return(
-        <Container>
+        <div style={{
+            marginBottom: "20px"
+        }}>
             <Row>
                 <Col>
                     <h4>{props.title}</h4>
                     <p>Author/s: {[props.authors].flat().join(", ")}</p>
                 </Col>
-                <Col><Button 
-                    href={props.link}
-                    target="_blank"
-                    >View</Button></Col>
-                <Col><Button
-                    onClick={handleClick()}
-                >Save</Button></Col>
+                <Col>
+                    <Button
+                        style={{
+                            marginLeft:"10px"
+                        }} 
+                        href={props.link}
+                        target="_blank"
+                    >View</Button>
+                    <Button
+                    onClick={handleSave}
+                    >Save</Button>
+                </Col>
             </Row>
             <Row>
                 <Col><img src={props.image} alt={props.title} /></Col>
                 <Col><p>{props.description}</p></Col>
             </Row>
-        </Container>
+        </div>
     )
 }
 
