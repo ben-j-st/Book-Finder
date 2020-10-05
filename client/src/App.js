9c04c5df2f514f895bbecf430046e66442e0ba84
Ben Stephens <ben_j_stephens@hotmail.com>
2020-10-05 15:31:50 +1100

    updating basic files

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,12 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-     
-    </div>
-  );
-}
-
-export default App;
+    return (
+        <div className="App">
+            <h1>This is Google Books</h1>
+        </div>
+        );
+    }
+    
+    export default App;
+    
