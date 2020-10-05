8471a234781061059d36caaaa1606d80c30a50d8
Ben Stephens <ben_j_stephens@hotmail.com>
2020-10-05 18:35:49 +1100

    fixed the search component

--- client/src/pages/search.js
+++ client/src/pages/search.js
@@ -1,47 +1,75 @@
-import { React, useState } from "react";
+import React from "react";
 
 import ResultContainer from "../components/resultContainer";
 
 import Jumbotron from "react-bootstrap/Jumbotron";
-import { Form, FormControl } from "react-bootstrap/";
+import { Form } from "react-bootstrap/";
 import Button from "react-bootstrap/Button"
+import API from "../util/API";
 
 
 function SearchPage() {
 
-    const [ books, setBooks] = useState([])
+    const [ books, setBooks] = React.useState([])
+    const [ input, setInput ] = React.useState('')
 
-        return(
-            <div>  
+    const runSearch = event => {
+        event.preventDefault();
+        API.searchBooks(input)
+        .then(res => {
+            console.log(res.data.items)
+            setBooks(res.data.items)
+        })
+    }
+
+    return (
+        <div>  
+            <Jumbotron>
+                <h4 style={{ textAlign:"left"}}>Book Search</h4>
+
+                <Form.Group 
+                    style={{
+                        display: "flex",
+                        justifyContent: "flex-end"
+                    }}>
+                    <Form.Control 
+                        type="text"
+                        value={input}
+                        onChange={e => setInput(e.target.value)} 
+                        placeholder="Please Enter A Book Title" 
+                    />
+                    <Button 
+                        style={{
+                            marginLeft: "5px"
+                        }}
+                        className="text-right"
+                        variant="outline-primary"
+                        onClick={runSearch}
+                    >Search</Button>
+                </Form.Group>
+
+            </Jumbotron>
+
+            {books.length ?  (
                 <Jumbotron>
-                    <h4>Book Search</h4>
-
-                    <Form inline>
-                        <FormControl type="text" placeholder="Please Enter A Book Title" className="mr-sm-2" />
-                        <Button variant="outline-primary">Search</Button>
-                    </Form>
-
+                    <h4 style={{ textAlign: "left"}}>Results</h4>
+                    {books.map((book) => (
+                        <ResultContainer
+                            key={book.id} 
+                            title={book.volumeInfo.title}
+                            authors={book.volumeInfo.authors}
+                            description={book.volumeInfo.description}
+                            image={book.volumeInfo.imageLinks.thumbnail}
+                            link={book.volumeInfo.previewLink}
+                        />
+                    ))}
                 </Jumbotron>
-
-                {books.length ?  (
-                    <Jumbotron>
-                        <h4>Results</h4>
-                        {books.map((book) => (
-                            <ResultContainer 
-                                title={book.title}
-                                authors={book.authors}
-                                description={book.description}
-                                image={book.image}
-                                link={book.link}
-                            />
-                        ))}
-                    </Jumbotron>
-                ) : (
-                    <h4>No Books to Display</h4>
-                )}
-                
-            </div>
-        )
+            ) : (
+                <h4>No Books to Display</h4>
+            )}
+            
+        </div>
+    )
 }
 
 export default SearchPage;
