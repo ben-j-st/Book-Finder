05353aec5ef5bf07ad0864ee980d21ee54c490a7
Ben Stephens <ben_j_stephens@hotmail.com>
2020-10-05 17:04:15 +1100

    updated to include routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,32 @@
 import React from 'react';
 import './App.css';
 
+import Navbar from "./components/navbar"
+import Header from "./components/header"
+
+import SearchPage from "./pages/search";
+import SavedPage from "./pages/saved";
+
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
+
 function App() {
     return (
-        <div className="App">
-            <h1>This is Google Books</h1>
+        <Router>
+            <div className="App">
+            <Navbar />
+            <Header />
+            <Switch>
+                <Route path="/saved">
+                    <SavedPage />
+                </Route>
+                <Route path="/">
+                    <SearchPage />
+                </Route>
+            </Switch>
         </div>
-        );
-    }
+        </Router>
+    );
+}
     
     export default App;
     
